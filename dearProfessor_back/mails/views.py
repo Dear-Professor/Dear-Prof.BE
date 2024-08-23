e8ef87ae853673d57777f98385955e40cbb6ea18
Hoyoung027 <hybyun0207@gmail.com>
2024-08-24 03:34:31 +0900

    temp-commit

--- dearProfessor_back/mails/views.py
+++ dearProfessor_back/mails/views.py
@@ -11,34 +11,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
-class SentEmailListView(viewsets.ViewSet):
-    queryset = SentEmail.objects.all
-    serializer_class = SentEmailSerializer
+from django.shortcuts import get_object_or_404
+
     
+# 메일 보내기. 
 class SendEmailView(APIView):
 
     def post(self, request):
-        # 사용자의 정보를 요청 데이터에서 가져오기
-        user = request.user
-        to_user = request.data.get('to_user')
-        subject = request.data.get('subject', 'No Subject')  # 과목명
-        title = request.data.get('title', 'No Title')  # 이메일 제목
-        final_context = request.data.get('final_context', '')
-
-        # SentEmail 객체 생성
-        email_data = {
-            # 'user': user.id,
-            # 'name': user.name,
-            'title': title,  # 이메일 제목
-            'final_context': final_context,
-            'to_user': to_user,
-            # 'is_feedback': request.data.get('is_feedback', False),
-            # 'feedback': request.data.get('feedback', ''),
-            # 'subject': subject  # 과목명 저장
-        }
-
-        serializer = SentEmailSerializer(data=email_data)
-        
+        serializer = SentEmailSerializer(data=request.data)
         if serializer.is_valid():
             from_email = serializer.validated_data['from_email']
             to_email = serializer.validated_data['to_email']
@@ -49,3 +29,49 @@ class SendEmailView(APIView):
 
             return Response({"message": "Email sent successfully"}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#보낸 메일 리스트/상세 조회
+class SentMailViewset(ModelViewSet):
+    queryset = SentEmail.objects.all()  # 기본 쿼리셋 정의
+    serializer_class = SentEmailViewSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+    #보낸메일리스트 가져오기 
+    @action(detail=False,methods=['get'] ,url_path='list_sentMail')
+    def list_sentmail(self,request,user_id=None):
+        user_id=int(request.query_params.get('user_id'))
+        authenticated_user_id = request.user.id
+        if not user_id:
+            return Response({"status": "error", "message": "month_id is required"}, status=400)
+        
+        if user_id != authenticated_user_id :
+            return Response({"status": "error", "message": "훔쳐보지마~!"}, status=401)
+        
+        queryset = self.queryset.filter(user_id=user_id)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"status": "success", "data": serializer.data},status=status.HTTP_200_OK)
+    
+    # #보낸 메일 상세 보기
+    @action(detail=False,methods=['get'] ,url_path='detail_sentMail')
+    def detail_sentmail(self,request,user_id=None,mail_id=None):
+        user_id=int(request.query_params.get('user_id'))
+        mail_id = request.query_params.get('mail_id')
+        authenticated_user_id = request.user.id
+        if not user_id:
+            return Response({"status": "error", "message": "month_id is required"}, status=400)
+        
+        if user_id != authenticated_user_id :
+            return Response({"status": "error", "message": "훔쳐보지마~!"}, status=401)
+        
+        if not mail_id:
+            return Response({"status": "error", "message": "mail_id is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        mail = get_object_or_404(SentEmail,id=mail_id,user_id=user_id)
+        serializer = self.get_serializer(mail)
+        return Response({"status":"success","data":  serializer.data}, status=status.HTTP_200_OK)
+
+
+  
